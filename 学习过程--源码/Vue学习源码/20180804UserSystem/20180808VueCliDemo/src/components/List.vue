bbcca01d001eb6f947e36b71db010bdd22900fac
罗康元 <243518985@qq.com>
2018-08-06 23:32:32 +0800

    格式再修改

--- 学习过程--源码/Vue学习源码/20180804UserSystem/20180808VueCliDemo/src/components/List.vue
+++ 学习过程--源码/Vue学习源码/20180804UserSystem/20180808VueCliDemo/src/components/List.vue
@@ -4,19 +4,19 @@
         <h2 style='display: none'>暂无评论，点击左侧添加评论！！！</h2>
         <ul class="list-group">
             <!-- 遍历父组件App传递的数据，将每一个数据传递给Item组件 -->
-          <Item v-for="(comment,index) in comments" :key="index" :comment = "comment"></Item>
+            <Item v-for="(comment,index) in comments" :key="index" :comment="comment"></Item>
         </ul>
     </div>
 </template>
 <script>
-import Item from './Item.vue' // 使用Item组件
-    export default {
-        // 声明接收父组件传递的数据属性，这个属性就会成为组件对象的属性
-        props: ['comments'],
-        components: {
-            Item
-        }
+import Item from "./Item.vue"; // 使用Item组件
+export default {
+    // 声明接收父组件传递的数据属性，这个属性就会成为组件对象的属性
+    props: ["comments"],
+    components: {
+        Item
     }
+};
 </script>
 <style>
 .reply {
