6683832ae4d9ac1cf08cf912ba5eb5617b0c80f5
罗康元 <243518985@qq.com>
2018-08-06 23:10:42 +0800

    静态组件初始化

--- 学习过程--源码/Vue学习源码/20180804UserSystem/20180808VueCliDemo/src/App.vue
+++ 学习过程--源码/Vue学习源码/20180804UserSystem/20180808VueCliDemo/src/App.vue
@@ -1,24 +1,37 @@
 <template>
-  <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <!-- 使用组件标签 -->
-    <HelloWorld/> 
-  </div>
+    <div id="app">
+        <header class="site-header jumbotron">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h1>请发表对Vue的评论</h1>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="container">
+            <Add/>
+            <List/>
+        </div>
+    </div>
 </template>
 
 <script>
 // 1.引入需要使用的vue组件（HelloWoed组件）
-import HelloWorld from './components/HelloWorld'
+import Add from "./components/Add";
+import List from "./components/List";
 
 export default {
-  components: { // 2.映射组件标签
-    HelloWorld
-  }
-}
+    components: {
+        // 2.映射组件标签
+        Add,
+        List
+    }
+};
 </script>
 
 <style>
-.logo{
+.logo {
     width: 100px;
     height: 100px;
 }
---
+++ 学习过程--源码/Vue学习源码/20180804UserSystem/20180808VueCliDemo/src/components/List.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="col-md-8">
+        <h3 class="reply">评论回复：</h3>
+        <h2 style='display: none'>暂无评论，点击左侧添加评论！！！</h2>
+        <ul class="list-group">
+            <li class="list-group-item">
+                <div class="handle">
+                    <a href="javascript:;">删除</a>
+                </div>
+                <p class="user">
+                    <span>xxx</span>
+                    <span>说:</span>
+                </p>
+                <p class="centence">React不错!</p>
+            </li>
+            <li class="list-group-item">
+                <div class="handle">
+                    <a href="javascript:;">删除</a>
+                </div>
+                <p class="user">
+                    <span>yyy</span>
+                    <span>说:</span>
+                </p>
+                <p class="centence">React有点难!</p>
+            </li>
+        </ul>
+    </div>
+</template>
+<style>
+.reply {
+    margin-top: 0px;
+}
+
+li {
+    transition: 0.5s;
+    overflow: hidden;
+}
+
+.handle {
+    width: 40px;
+    border: 1px solid #ccc;
+    background: #fff;
+    position: absolute;
+    right: 10px;
+    top: 1px;
+    text-align: center;
+}
+
+.handle a {
+    display: block;
+    text-decoration: none;
+}
+
+.list-group-item .centence {
+    padding: 0px 50px;
+}
+
+.user {
+    font-size: 22px;
+}
+</style>
+
