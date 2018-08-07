2cfccbda34e19867e0111bd9652733fa412a4464
罗康元 <243518985@qq.com>
2018-08-07 22:25:24 +0800

    添加事件绑定

--- 学习过程--源码/Vue学习源码/20180804UserSystem/20180808VueCliDemo/src/components/Add.vue
+++ 学习过程--源码/Vue学习源码/20180804UserSystem/20180808VueCliDemo/src/components/Add.vue
@@ -3,17 +3,36 @@
                 <form class="form-horizontal">
                     <div class="form-group">
                         <label>用户名</label>
-                        <input type="text" class="form-control" placeholder="用户名">
+                        <input type="text" class="form-control" placeholder="用户名" v-model="name">
                     </div>
                     <div class="form-group">
                         <label>评论内容</label>
-                        <textarea class="form-control" rows="6" placeholder="评论内容"></textarea>
+                        <textarea class="form-control" rows="6" placeholder="评论内容" v-model="content"></textarea>
                     </div>
                     <div class="form-group">
                         <div class="col-sm-offset-2 col-sm-10">
-                            <button type="button" class="btn btn-default pull-right">提交</button>
+                            <button type="button" class="btn btn-default pull-right" @click = "add">提交</button>
                         </div>
                     </div>
                 </form>
             </div>
 </template>
+<script>
+    export default {
+        data() {
+            return {
+                 name: '',
+                content: ''
+            }
+        },
+        methods: {
+            add(){
+                // 检查输入的合法性
+                const name = this.name.trim();
+                const content = this.content.trim();
+                // 根据输入的数据，封装成个comment对象
+                // 添加到comments中
+            }
+        }
+    }
+</script>
